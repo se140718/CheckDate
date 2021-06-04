765f7afdc992d377c38c027711fcc27bdb9774ef
Dat Lu <60058760+se140718@users.noreply.github.com>
2021-06-04 17:15:06 +0700

    Add files via upload

---
+++ Lab1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.day = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.month = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.year = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.fpt;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 59);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date Time Checker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(109, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Day";
+            // 
+            // day
+            // 
+            this.day.Location = new System.Drawing.Point(161, 154);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(100, 20);
+            this.day.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(109, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Month";
+            // 
+            // month
+            // 
+            this.month.Location = new System.Drawing.Point(161, 190);
+            this.month.Name = "month";
+            this.month.Size = new System.Drawing.Size(100, 20);
+            this.month.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(109, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Year";
+            // 
+            // year
+            // 
+            this.year.Location = new System.Drawing.Point(161, 223);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(100, 20);
+            this.year.TabIndex = 7;
+            // 
+            // btnClear
+            // 
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(96, 260);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 22);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.ForeColor = System.Drawing.Color.Black;
+            this.btnCheck.Location = new System.Drawing.Point(203, 260);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(84, 22);
+            this.btnCheck.TabIndex = 9;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 339);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.month);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.day);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox day;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox month;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox year;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ Lab1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Regex dayRegex = new Regex("[0-9]+");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int DaysInMonth(int year, int month)
+        {
+            int dim = year / month;
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                case 2:
+                    if (year % 400 == 0)
+                    {
+                        return 29;
+                    }
+                    else
+                    {
+                        if (year % 100 == 0)
+                        {
+                            return 28;
+                        }
+                        else
+                        {
+                            if (year % 4 == 0)
+                            {
+                                return 29;
+                            }
+                            else
+                            {
+                                return 28;
+                            }
+                        }
+                    }
+                default:
+                    return 0;
+            }
+        }
+
+        public Boolean IsValidDate(int year, int month, int day)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                if (day >= 1)
+                {
+                    if (day <= DaysInMonth(year, month))
+                    {
+                        return true;
+                    }
+                    else return false;
+                }
+                else return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            day.Text = "";
+            month.Text = "";
+            year.Text = "";
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int y = int.Parse(year.Text);
+            int m = int.Parse(month.Text);
+            int d = int.Parse(day.Text);
+            try
+            {
+                if (!dayRegex.IsMatch(day.Text))
+                {
+                    MessageBox.Show("Input data for Day is incorrect format!");
+                }
+                if (!dayRegex.IsMatch(month.Text))
+                {
+                    MessageBox.Show("Input data for Month is incorrect format!");
+                }
+                if (!dayRegex.IsMatch(year.Text))
+                {
+                    MessageBox.Show("Input data for Year is incorrect format!");
+                }
+                if (d < 1 && d > 31)
+                {
+                    MessageBox.Show("Input data for Day is out of range!");
+                }
+                if (m < 1 && d > 12)
+                {
+                    MessageBox.Show("Input data for Month is out of range!");
+                }
+                if (y < 1000 && y > 3000)
+                {
+                    MessageBox.Show("Input data for Year is out of range!");
+                }
+                if (IsValidDate(y, m, d))
+                {
+                    MessageBox.Show(day.Text + "/" + month.Text + "/" + year.Text + " is correct day time!");
+                }
+                else
+                {
+                    MessageBox.Show(day.Text + "/" + month.Text + "/" + year.Text + " is NOT correct day time!");
+                }
+            }
+            catch (Exception) { }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit?", "My Application",
+            MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
